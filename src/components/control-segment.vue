c4a005a99891c6c9998f7e4da5ec6deec36acb80
Daniel Howard <dan@fullgun.com>
2017-11-14 08:16:12 +0000

    Leave out arc handles for now, not good enough

--- src/components/control-segment.vue
+++ src/components/control-segment.vue
@@ -6,8 +6,8 @@
 		<template v-for="marker in this.markers">
 			<h-segment-end-marker v-if="marker.type == 'h-end'" :segmentAddr="addr" :key="marker.key" />
 			<h-segment-end-marker v-if="marker.type == 'v-end'" :segmentAddr="addr" :key="marker.key" />
-			<a-segment-radius-marker v-if="marker.type == 'arc-rad'" :segmentAddr="addr" :key="marker.key" />
-			<a-segment-rotation-marker v-if="marker.type == 'arc-rot'" :segmentAddr="addr" :key="marker.key" />
+			<!--<a-segment-radius-marker v-if="marker.type == 'arc-rad'" :segmentAddr="addr" :key="marker.key" />-->
+			<!--<a-segment-rotation-marker v-if="marker.type == 'arc-rot'" :segmentAddr="addr" :key="marker.key" />-->
 		</template>
 	</g>
 </template>
@@ -17,12 +17,12 @@ import PointMarker from './point-marker.vue';
 import GuideLine from './control-guide-line.vue';
 import HSegmentEndMarker from './h-segment-end-marker.vue';
 import VSegmentEndMarker from './v-segment-end-marker.vue';
-import ASegmentRadiusMarker from './a-segment-radius-marker.vue';
-import ASegmentRotationMarker from './a-segment-rotation-marker.vue';
+//import ASegmentRadiusMarker from './a-segment-radius-marker.vue';
+//import ASegmentRotationMarker from './a-segment-rotation-marker.vue';
 import wrappers from '../util/wrappers.js';
 export default {
 	props: ['addr','pathSelected'],
-	components: { GuideLine, PointMarker, HSegmentEndMarker, VSegmentEndMarker, ASegmentRadiusMarker, ASegmentRotationMarker },
+	components: { GuideLine, PointMarker, HSegmentEndMarker, VSegmentEndMarker/*, ASegmentRadiusMarker, ASegmentRotationMarker*/ },
 	computed: {
 		points: function() {
 			var segmentData = this.$store.state.drawing.segments[this.addr];
@@ -54,7 +54,7 @@ export default {
 					end: {x: segment.c1.x, y: segment.c1.y}
 				});
 			}
-			if (segment.data.type.toLowerCase() == 'a') {
+			/*if (segment.data.type.toLowerCase() == 'a') {
 				segment.setArcData();
 				guideLines.push({
 					key:'a0-guide/'+this.addr,
@@ -76,7 +76,7 @@ export default {
 					start: segment.vertex1,
 					end: segment.radiusHandle
 				});
-			}
+			}*/
 			return guideLines;
 		},
 		markers: function() {
@@ -89,10 +89,10 @@ export default {
 				case 'v':
 					markers.push({type: 'v-end', key:'v-end/'+this.addr });
 					break;
-				case 'a':
+				/*case 'a':
 					markers.push({type: 'arc-rad', key:'arc-rad/'+this.addr });
 					markers.push({type: 'arc-rot', key:'arc-rot/'+this.addr });
-					break;
+					break;*/
 
 			}
 			return markers;
